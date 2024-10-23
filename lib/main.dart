5cd7ae445294bcd896803f329340dc8da14c56ff
Karlo Kraml <karlokraml2@gmail.com>
2024-10-23 16:12:28 +0200

    added chips and user fields

--- lib/main.dart
+++ lib/main.dart
@@ -29,19 +29,124 @@ class HomePage extends StatelessWidget {
             child: Column(
           children: [
             MyAppBar(),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
-              child: SearchBar(
-                leading: const Icon(Icons.search, color: Colors.black,),
-                hintText: 'Search...',
-                
-                backgroundColor: const WidgetStatePropertyAll(Colors.white),
-                shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-              ),
-            )
+            MySearchBar(),
+            Chips(),
+            Expanded(
+                child: ListView(
+              padding: EdgeInsets.all(8),
+              children: [
+                UserContainer(),
+                SizedBox(
+                  height: 20,
+                ),
+                UserContainer(),
+                SizedBox(
+                  height: 20,
+                ),
+                UserContainer(),
+                SizedBox(
+                  height: 20,
+                ),
+                UserContainer(),
+                SizedBox(
+                  height: 20,
+                ),
+                UserContainer()
+              ],
+            ))
           ],
         )),
       ),
+    );
+  }
+}
+
+class MySearchBar extends StatelessWidget {
+  const MySearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+      child: SearchBar(
+        leading: const Icon(
+          Icons.search,
+          color: Colors.black,
+        ),
+        hintText: 'Search...',
+        backgroundColor: const WidgetStatePropertyAll(Colors.white),
+        shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+      ),
+    );
+  }
+}
+
+class UserContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      height: 100,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ProfilePhoto(
+              totalWidth: 70,
+              cornerRadius: 80,
+              color: Colors.blue,
+              image: const AssetImage('assets/profile_picture.jpg'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Name and Surname',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text('Text here'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Chips extends StatelessWidget {
+  final List<String> chipLabels = [
+    'Offline',
+    'Online',
+    'Parental',
+    'Sick',
+    'Something'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: chipLabels.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Chip(
+                label: Text(chipLabels[index]),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+            );
+          }),
     );
   }
 }
